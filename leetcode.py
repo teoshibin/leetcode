eeaabe0308d206d6778e06db71d5d9c8eb668afd
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2023-05-31 01:37:31 +0100

    abstract test class

--- leetcode.py
+++ leetcode.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import json
 
+from AbstractSolution import AbstractSolution
+
 HISTORY_FILE = "history.json"
 PROBLEMS_DIR = "problems"
 TEMPLATE_FILE = "template.py"  # This is the template file you should create
@@ -18,17 +20,17 @@ def create_problem_file(problem_number):
         file.write(template)
 
 
-def update_history(problem_number):
-    with open(HISTORY_FILE, "w") as file:
-        json.dump({"last_problem": problem_number}, file)
+# def update_history(problem_number):
+#     with open(HISTORY_FILE, "w") as file:
+#         json.dump({"last_problem": problem_number}, file)
 
 
-def get_last_problem():
-    if os.path.exists(HISTORY_FILE):
-        with open(HISTORY_FILE, "r") as file:
-            data = json.load(file)
-            return data.get("last_problem")
-    return None
+# def get_last_problem():
+#     if os.path.exists(HISTORY_FILE):
+#         with open(HISTORY_FILE, "r") as file:
+#             data = json.load(file)
+#             return data.get("last_problem")
+#     return None
 
 
 def run_test(problem_number, solution_number=None):
@@ -58,18 +60,18 @@ def run_solution(problem_number, solution_number=None):
 
         # Get the solution classes
         solution_classes = [cls for cls in module.__dict__.values() if isinstance(
-            cls, type) and cls.__module__ == module.__name__]
+            cls, type) and issubclass(cls, AbstractSolution) and cls.__module__ == module.__name__]
 
         if solution_number is not None:
-            solution_classes = [cls for cls in solution_classes if cls.__name__.endswith(
-                f"Solution{solution_number}")]
+            solution_classes = [cls for cls in solution_classes if cls.number == solution_number] # type: ignore
 
         for solution_class in solution_classes:
             solution = solution_class()
-            solution.run()  # This is the new preserved 'run' method
+            solution.run()
 
     except ModuleNotFoundError:
         print(f"Problem {problem_number} not found")
+
 
 
 def main():
@@ -83,30 +85,31 @@ def main():
                         help='Specify a solution number.')
     parser.add_argument('-c', '--create', type=int,
                         help='Create a new problem file from template.')
-    parser.add_argument('-u', '--update', type=int,
-                        help='Update the history to specific problem number.')
+    # parser.add_argument('-u', '--update', type=int,
+    #                     help='Update the history to specific problem number.')
 
     args = parser.parse_args()
 
     if args.test:
         run_test(args.test, args.solution)
-        update_history(args.test)
+        # update_history(args.test)
     elif args.run:
         run_solution(args.run, args.solution)
-        update_history(args.run)
+        # update_history(args.run)
     elif args.create:
         create_problem_file(args.create)
         print(f"Problem {args.create} has been created.")
-    elif args.update:
-        update_history(args.update)
-        print(f"History updated to number {args.update}.")
+    # elif args.update:
+        # update_history(args.update)
+        # print(f"History updated to number {args.update}.")
     else:
-        last_problem = get_last_problem()
-        if last_problem:
-            print(f"Running last executed problem: {last_problem}")
-            run_test(last_problem)
-        else:
-            print("No problem number provided and history not found.")
+        # last_problem = get_last_problem()
+        # if last_problem:
+        #     print(f"Running last executed problem: {last_problem}")
+        #     run_test(last_problem)
+        # else:
+        #     print("No problem number provided and history not found.")
+        print("No problem number provided.")
 
 
 # Main method
--- problems/problem_0.py
+++ problems/problem_0.py
@@ -5,20 +5,20 @@ class Solution1(AbstractSolution):
     number = AbstractSolution.next_solution()
 
     def solve(self, input):
-        pass
+        return 1
     
     def run(self):
-        pass
+        print("hello" + str(self.number))
 
 
 class Solution2(AbstractSolution):
     number = AbstractSolution.next_solution()
 
     def solve(self, input):
-        pass
+        return 2
     
     def run(self):
-        pass
+        print("hello" + str(self.number))
 
 
 class TestProblem(AbstractTestProblem):
@@ -27,7 +27,11 @@ class TestProblem(AbstractTestProblem):
 
     def test_case1(self):
         input = ""
-        expected_output = ""
+        expected_output = 3
         self.run_test_case(input, expected_output)
 
+    def test_case2(self):
+        input = ""
+        expected_output = 2
+        self.run_test_case(input, expected_output)
 
